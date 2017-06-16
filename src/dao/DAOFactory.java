4fe6356ae5052cbdec9c352427a1916057dd38fc
Explorer Zhang <zbqhc@163.com>
2017-06-16 13:08:37 +0800

    update readme

--- src/dao/DAOFactory.java
+++ src/dao/DAOFactory.java
@@ -6,33 +6,34 @@ import java.util.Properties;
 //参考资料：http://blog.csdn.net/nuowei_senlin/article/details/57073655
 public class DAOFactory {
 
-	private static Properties prop = new Properties();  
-        try {  
-            prop.load(DAOFactory.class.getClassLoader().getResourceAsStream("daofactory.properties"));  
-            System.out.println(DAOFactory.class.getClassLoader());
-        } catch (IOException e) {  
-            throw new RuntimeException(e);  
-        }  
-     
-	
+	private static Properties prop = new Properties();
+
 	@SuppressWarnings("unchecked")
-	public static <T> T getDAO(Class <T> interfaceClass){
-		String simplename = interfaceClass.getSimpleName();  
-        System.out.println("简单类名是："+simplename);    //UserDao  
-        String classname = prop.getProperty(simplename);   
+	public static <T> T getDAO(Class<T> interfaceClass) {
+		try {
+			prop.load(DAOFactory.class.getClassLoader().getResourceAsStream(
+					"daofactory.properties"));
+		} catch (IOException e1) {
+			// TODO 自动生成的 catch 块
+			e1.printStackTrace();
+		}
+		System.out.println(DAOFactory.class.getClassLoader());
+		String simplename = interfaceClass.getSimpleName();
+		System.out.println("简单类名是：" + simplename); // UserDao
+		String classname = prop.getProperty(simplename);
 		System.out.println(interfaceClass);
-		
-		
-		try{
+
+		try {
 			return (T) Class.forName("dao.BasicInfo").newInstance();
-		}catch (Exception e){
+		} catch (Exception e) {
 			System.out.println();
 			throw new ExceptionInInitializerError(e);
 		}
 	}
-      public static void main(String[] args) {
-		//System.out.println(new BasicInfo());
-		BasicInfo lloDao =(BasicInfo) DAOFactory.getDAO(BasicInfoDAO.class);
-		}
-	
+
+	public static void main(String[] args) {
+		// System.out.println(new BasicInfo());
+		BasicInfo lloDao = (BasicInfo) DAOFactory.getDAO(BasicInfoDAO.class);
+	}
+
 }
