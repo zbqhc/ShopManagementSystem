e97ee21530dda745e3ec828e2732f0cd4352b121
Explorer Zhang <zbqhc@163.com>
2017-06-17 19:27:20 +0800

    add userdaolmpl

--- src/demo/HowToUse.java
+++ src/demo/HowToUse.java
@@ -9,7 +9,7 @@ public class HowToUse {
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws Exception {
 		UserInfoDAO userInfoDAO = DAOFactory.getDAO("登录信息");
-//		userInfoDAO.addUser(1520010900,"软件15-9","lntu",15);//添加用户
+		userInfoDAO.addUser(1520010900,"软件15-9","lntu",15);//添加用户
 //		userInfoDAO.delUser(1520010924);
 //		userInfoDAO.updateUser(1520010924, "new", 9999);
 		for (UserInfo i:(List<UserInfo>) userInfoDAO.queryAll()){
