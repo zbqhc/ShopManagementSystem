aba8468eaa5d50e05c950cd97854d6510004ac87
Explorer Zhang <zbqhc@163.com>
2017-06-20 14:46:11 +0800

    添加数据库登录测试项目

--- src/demo/HowToUse.java
+++ src/demo/HowToUse.java
@@ -9,18 +9,10 @@ public class HowToUse {
 
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws Exception {
-		PaymentInfoDAO paymentInfoDAO = DAOFactory.getDAO("账单信息");// 从dao工厂获取dao的实现对象
+		UserInfoDAO userInfoDAO = DAOFactory.getDAO("登录信息");// 从dao工厂获取dao的实现对象
 
-		 paymentInfoDAO.addRecord(new PaymentInfo(1520010923, 2323, "显示器", 200, (float) 3000.5,  "无" ));
-		 paymentInfoDAO.addRecord(new PaymentInfo(1520010924, 2323, "显示器", 200, (float) 3000.5,  "无" ));
-		 paymentInfoDAO.addRecord(new PaymentInfo(1520010927, 2323, "显示器", 200, (float) 300.5,  "无" ));
-		 paymentInfoDAO.addRecord(new PaymentInfo(1520010928, 2323, "显示器", 200, (float) 300.5,  "无" ));
-//		 paymentInfoDAO.delRecord(1520010923);
-		paymentInfoDAO.calcProfitByDate("2017/06/19");
-		 for (PaymentInfo i:(List<PaymentInfo>) paymentInfoDAO.QueryByDate("2017/06/19")){
-			 System.out.print(i.getId());
-			 System.out.println(i.getTimeStamp());
-			 
-		 }
+		userInfoDAO.addUser(new UserInfo(1520010920, "szz", "1", 528));
+
 	}
+
 }
